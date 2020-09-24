6883746a083f5427e97efc845b247b0cdde0eaf4
Voya96 <shavoya.beecher@gmail.com>
2020-09-24 02:31:41 -0400

    added the questions in app.js

---
+++ app.js
@@ -0,0 +1,64 @@
+
+
+const quiz = [
+    {
+        question: "Why did Johnny orignally purchase the town of Schitt's Creek?",
+        choice: ["As a joke for Alexis", "As another location for Rose video", "As a wedding gift to Moria", "As a joke for David"],
+        answer: 3,
+        photo:
+    }
+    {
+        question: "What does the sign that Roland adds to the town sign say?",
+        choice: ["Horace Schitt welcomes you home", "It's not polite to stare!", "Don't worry, it's his sister", "Don't worry, we're family"],
+        answer: 2,
+        photo:
+    }
+    {
+        question: "What does the type on Alexis' Elmdale college diploma say?",
+        choice: ["Alex Rose", "Alexis Rosé", "Pubic relations", "Public Relationships"],
+        answer: 2,
+        photo:
+    }
+    {
+        question: "What does Moira say was handed down to the women in her family as emergency currency incase they need to leave their husbands in the night?",
+        choice: ["A brown crocodile handbag", "An enchilada recipe", "The address of a cobin in the woods", "A suitcase filled with American Dollars"],
+        answer: 0,
+        photo:
+    }
+    {
+        question: "What illegal business does Johnny try to start in Season 2?",
+        choice: ["Raising tigers", "A casino", "Selling raw milk", "Selling counterfit bags"],
+        answer: 2,
+        photo:
+    }
+    {
+        question: "Which celebrity did Alexis say she used to text for a booty call?",
+        choice: ["Ryan Reynolds", "Leo DiCaprio", "Brad Pitt", "Zac Efron"],
+        answer: 3,
+        photo:
+    }
+    {
+        question: "Who said, "/"I could not be more at one with nature. I do Coachella every year"?",
+        choice: ["Alexis", "Moria", "David", "Johnny"],
+        answer: 2,
+        photo:
+    }
+    {
+        question: "Complete the quote: "/"I do drink red wine, but I also drink white wine. And I've been known to sample the occasional Rosé. And a couple summers back, I tried a ____ that used to be a Chardonnay, which got a bit complicated. I like the wine, not the label.",
+        choice: ["Cabernet", "Merlot", "Pinot Noir", "Malbec"],
+        answer: 1,
+        photo:
+    }
+    {
+        question: "Complete the quote: "/"I once planned Megan Fox's bachelorette on this tiny island off the coast of ____ where nothing is illegal. Like, nothing.",
+        choice: ["Romania", "Sierra Leone", "Madagascar", "Montenegro"]
+        answer: 3,
+        photo:
+    }
+    {
+        question: "For the most part, Alexis has a teasing relationship with her brother. What has become her signature line when talking to him?",
+        choice: ["Ew, David!", "As if!", "David, what are you doing?", "Schwing"],
+        answer: 0,
+        photo:
+    }
+]
---
+++ notes.md
@@ -0,0 +1,25 @@
+Questions
+
+Question 1: Why did Johnny orignally purchase the town of Schitt's Creek?
+Question 2: What does the sign that Roland adds to the town sign say?
+Question 3: What does the type on Alexis' Elmdale college diploma say?
+Question 4: What does Moira say was handed down to the women in her family as emergency currency incase they need to leave their husbands in the night?
+Question 5: What illegal business does Johnny try to start in Season 2?
+Question 6: Which celebrity did Alexis say she used to text for a booty call?
+Question 7: Who said, "I could not be more at one with nature. I do Coachella every year"?
+Question 8: Complete the quote: "I do drink red wine, but I also drink white wine. And I've been known to sample the occasional Rosé. And a couple summers back, I tried a ____ that used to be a Chardonnay, which got a bit complicated. I like the wine, not the label." — David Rose
+Question 9: Complete the quote: "I once planned Megan Fox's bachelorette on this tiny island off the coast of ____ where nothing is illegal. Like, nothing." — Alexis Rose
+Question 10: For the most part, Alexis has a teasing relationship with her brother. What has become her signature line when talking to him?
+
+Options
+
+Options 1: As a joke for Alexis, As another location for Rose video, As a wedding gift to Moria, As a joke for David.
+Option 2: "Horace Schitt welcomes you home", "It's not polite to stare!", "Don't worry, it's his sister", "Don't worry, we're family"
+Option 3: Alex Rose, Alexis Rosé, Pubic relations, Public Relationships
+Option 4: A brown crocodile handbag, An enchilada recipe, The address of a cobin in the woods, A suitcase filled with American Dollars
+Option 5: Raising tigers, A casino, Selling raw milk, Selling counterfit bags
+Option 6: Ryan Reynolds, Leo DiCaprio, Brad Pitt, Zac Efron
+Option 7: Alexis, Moria, David, Johnny
+Option 8: Cabernet, Merlot, Pinot Noir, Malbec
+Option 9: Romania, Sierra Leone, Madagascar, Montenegro
+Option 10: "Ew, David!", "As if!", "David, what are you doing?", "Schwing"
